94da08cf9ed7fa738d0a6c9b1e9f52cc47796777
iamitdubey <61047246+iamitdubey@users.noreply.github.com>
2020-02-16 22:06:57 +0530

    Add files via upload

---
+++ ImportProducts.Service/IImportProductService.cs
@@ -0,0 +1,14 @@
+﻿using ImportProducts.Service.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImportProducts.Service
+{
+    public interface IImportProductService
+    {
+        ImportProductLog ImportProductLog { get; set; }
+        Task ProcessImport(); // Download file, process/transform and store in the DB
+    }
+}
---
+++ ImportProducts.Service/ILoggerService.cs
@@ -0,0 +1,14 @@
+﻿using ImportProducts.Service.Model;
+using System;
+using System.Threading.Tasks;
+
+namespace ImportProducts.Service
+{
+    public interface ILoggerService
+    {
+        Task<decimal> InfoLogInsert(ImportProductLog importProductLogLog);
+        Task InfoLogUpdate(ImportProductLog importProductLogLog);
+        Task ErrorLogUpdate(ImportProductLog importProductLogLog);
+        Task InsertError(ImportProductLog importProductLogLog);
+    }
+}
