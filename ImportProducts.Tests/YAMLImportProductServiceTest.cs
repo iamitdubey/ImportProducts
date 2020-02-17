875b74cc5b5394a89c1059fa78e93a10c28c6d5c
Amit Dubey <amit.dubey@onsolve.com>
2020-02-17 19:17:03 +0530

    Adding tests leftover file

---
+++ ImportProducts.Tests/YAMLImportProductServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using ImportProducts.Service;
+using Microsoft.Extensions.Logging;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
+
+namespace ImportProducts.Tests
+{
+    [TestClass()]
+    public class YAMLImportProductServiceTest
+    {
+        private Mock<ILogger<YAMLImportProductService>> _logger;
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            //initialize all the mocks here
+            _logger = new Mock<ILogger<YAMLImportProductService>>();
+        }
+
+        [TestMethod()]
+        public void ProcessImportTest()
+        {
+            var result = true;
+            Assert.IsTrue(result == true);
+        }
+    }
+}
